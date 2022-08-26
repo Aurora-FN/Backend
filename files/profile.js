015471ea376be443c19c102ec64c4039da0025eb
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-08-26 20:28:04 +0100

    Add files via upload

--- files/profile.js
+++ files/profile.js
@@ -3,6 +3,17 @@ const app = express.Router();
 
 const fs = require("fs")
 
-
+app.all("/fortnite/api/game/v2/profile/:accountId/*/:command", async (req, res) => {
+    var retJSON = {
+        profileRevision: rvn + 1,
+        profileId: profileID,
+        profileChangesBaseRevision: 1,
+        profileChanges: [],
+        profileCommandRevision: 1,
+        serverTime: new Date(),
+        responseVersion: 1
+    }
+    res.json(retJSON)
+})
 
 module.exports = app
