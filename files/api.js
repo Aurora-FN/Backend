99ff83688f0a16d7179fdea5c72048e977f65e49
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-08-28 01:18:07 +0100

    Add files via upload

--- files/api.js
+++ files/api.js
@@ -5,7 +5,19 @@ const app = express.Router();
 const fs = require("fs")
 
 app.get("/fortnite/api/cloudstorage/system", (req, res) => {
-    res.setHeader('content-type', 'application/json').json([]).status(200)
+    res.setHeader('content-type', 'application/json').json([
+        {
+            "uniqueFilename": "DefaultEngine.ini",
+            "filename": "DefaultEngine.ini",
+            "hash": "603E6907398C7E74E25C0AE8EC3A03FFAC7C9BB4",
+            "hash256": "973124FFC4A03E66D6A4458E587D5D6146F71FC57F359C8D516E0B12A50AB0D9",
+            "length": fs.readFileSync("./cloudstorage/DefaultEngine.ini").length,
+            "contentType": "text/plain",
+            "uploaded": "9999-9999-9999",
+            "storageType": "S3",
+            "doNotCache": false
+        }
+    ]).status(200)
 })
 app.get("/fortnite/api/cloudstorage/system/config", (req, res) => {
     res.setHeader('content-type', 'application/json').json([]).status(200);
@@ -13,6 +25,17 @@ app.get("/fortnite/api/cloudstorage/system/config", (req, res) => {
 app.put("/fortnite/api/cloudstorage/user/:accountId/:filename", (req, res) => {
     res.status(204).send()
 })
+app.get("/fortnite/api/cloudstorage/system/:file", (req, res) => {
+    const file = req.params.file;
+    try {
+        res.sendFile(path.join(__dirname, `../cloudstorage/${file}`));
+        res.status(200);
+    } catch (err) {
+        console.log(err);
+        res.status(200);
+    }
+})
+
 app.get("/fortnite/api/storefront/v2/keychain", (req, res) => {
     res.json(JSON.parse(fs.readFileSync(`./json/keychain.json`, 'utf8'))).status(200);
 })
@@ -50,7 +73,9 @@ app.get("/account/api/epicdomains/ssodomains", (req, res) => {
         "epicgames.com"
     ])
 })
-app.get("/api/v1/events/Fortnite/download/*", async (req, res) => {
-    res.json({})
-})
+app.get("/api/v1/events/Fortnite/download/*", async (req, res) => res.json({}))
+app.get("/fortnite/api/game/v2/world/info", (req, res) => res.json({}))
+app.get("/fortnite/api/game/v2/twitch/*", (req, res) => res.json([]))
+app.get("/eulatracking/api/shared/agreements/fn", (req, res) => res.json({}))
+app.all("/fortnite/api/receipts/v1/account/*/receipts", (req, res) => res.json({}))
 module.exports = app
