74e38ab0bd301f829a154f36fcf8b699478c1475
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-08-29 23:40:01 +0100

    content

--- files/content.js
+++ files/content.js
@@ -2,9 +2,60 @@ const express = require("express")
 const app = express.Router();
 
 const fs = require("fs")
-
+const path = require("path")
 app.get(["/content/api/pages/fortnite-game", "/content/app/pages/"], (req, res) => {
-    res.json(JSON.parse(fs.readFileSync(`./json/content.json`, 'utf8')))
+    const config = JSON.parse(fs.readFileSync(path.join(__dirname,`../json/content.json`), 'utf8'))
+    console.log(config)
+    res.json({
+        "jcr:isCheckedOut": true,
+        _title: "Fortnite Game",
+        "jcr:baseVersion": "a7ca237317f1e7883b3279-c38f-4aa7-a325-e099e4bf71e5",
+        _activeDate: "2017-08-30T03:20:48.050Z",
+        lastModified: new Date(),
+        _locale: "en-US",
+        battleroyalenewsv2: {
+            news: {
+                motds: [
+                    {
+                        entryType: "Text",
+                        image: config.image,
+                        tileImage: config.tileImage,
+                        hidden: false,
+                        videoMute: false,
+                        tabTitleOverride: "Aurora",
+                        _type: "CommonUI Simple Message MOTD",
+                        title: config.title,
+                        body: config.body,
+                        videoLoop: false,
+                        videoStreamingEnabled: false,
+                        sortingPriority: 0,
+                        id: `AuroraNews`,
+                        spotlight: false
+                    }
+                ]
+            }
+        },
+        emergencynoticev2: {
+            news: {
+                platform_messages: [],
+                _type: "Battle Royale News",
+                messages: [
+                    {
+                        hidden: false,
+                        _type: "CommonUI Simple Message Base",
+                        subgame: "br",
+                        title: config.alert,
+                        body: config.alertmessage,
+                        spotlight: true
+                    }
+                ]
+            },
+            _title: "emergencynotice",
+            _activeDate: new Date(),
+            lastModified: new Date(),
+            _locale: "en-US"
+        },
+    })
 })
 app.get('/content/*', function (req, res) {
     res.status(404).json({
