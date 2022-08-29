291835638c727ae20e3e892ff9c264035154b435
zinx <63958372+zinx-YT@users.noreply.github.com>
2022-08-29 18:25:41 +0100

    Add files via upload

--- xmpp/index.js
+++ xmpp/index.js
@@ -2,6 +2,8 @@ const WebSocket = require('ws');
 
 const wss = new WebSocket.Server({ server });
 
+global.Clients = [];
+
 wss.on('listening', ws => {
     console.log(`XMPP started listening`)
 })
@@ -10,6 +12,10 @@ wss.on("connection", ws => {
     console.log("New XMPP Connection")
     ws.on("close", async (lol) => {
         console.log("Lost XMPP Connection")
+    })
+
+    ws.on('message', (msg) => {
+        console.log(msg)
     })
 })
 
